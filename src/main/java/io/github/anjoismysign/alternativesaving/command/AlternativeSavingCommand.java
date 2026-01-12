637ee36bd282a4703ba512edd2e4476712ad3830
anjoismysign <lubensaico@gmail.com>
2026-01-12 00:17:55 -0600

    Add profile command and language entry for Added-Profile message.

--- src/main/java/io/github/anjoismysign/alternativesaving/command/AlternativeSavingCommand.java
+++ src/main/java/io/github/anjoismysign/alternativesaving/command/AlternativeSavingCommand.java
@@ -1,8 +1,11 @@
 package io.github.anjoismysign.alternativesaving.command;
 
 import io.github.anjoismysign.alternativesaving.AlternativeSaving;
+import io.github.anjoismysign.alternativesaving.configuration.SavingConfiguration;
 import io.github.anjoismysign.alternativesaving.director.manager.AlternativeSavingManager;
+import io.github.anjoismysign.alternativesaving.director.manager.ConfigurationManager;
 import io.github.anjoismysign.alternativesaving.entity.SerialPlayer;
+import io.github.anjoismysign.alternativesaving.entity.SerialProfile;
 import io.github.anjoismysign.bloblib.api.BlobLibMessageAPI;
 import io.github.anjoismysign.skeramidcommands.command.Command;
 import io.github.anjoismysign.skeramidcommands.command.CommandBuilder;
@@ -13,18 +16,54 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.UUID;
+
 public enum AlternativeSavingCommand {
     INSTANCE;
 
     public void load(){
-        Command alternativesaving = CommandBuilder.of("alternativesaving").build();
-        switchProfile(alternativesaving);
+        Command alternativeSaving = CommandBuilder.of("alternativesaving").build();
+        Command profileCommand = alternativeSaving.child("profile");
+        switchProfile(profileCommand);
+        addProfile(profileCommand);
     }
 
-    public void switchProfile(Command alternativesaving){
-        Command profileCommand = alternativesaving.child("profile");
-        profileCommand.setParameters(BukkitCommandTarget.ONLINE_PLAYERS());
-        profileCommand.onExecute((permissionMessenger, args) -> {
+    public void addProfile(Command profileCommand){
+        Command command = profileCommand.child("add");
+        command.setParameters(BukkitCommandTarget.ONLINE_PLAYERS());
+        command.onExecute((permissionMessenger, args) -> {
+            CommandSender sender = BukkitAdapter.getInstance().of(permissionMessenger);
+            Player target = BukkitCommandTarget.ONLINE_PLAYERS().parse(args[0]);
+            if (target == null) {
+                BlobLibMessageAPI.getInstance()
+                        .getMessage("Player.Not-Found", sender)
+                        .toCommandSender(sender);
+                return;
+            }
+            @Nullable SerialPlayer serialPlayer = AlternativeSavingManager.getSerialPlayer(target);
+            if (serialPlayer == null){
+                BlobLibMessageAPI.getInstance()
+                        .getMessage("Player.Not-Inside-Plugin-Cache", target)
+                        .toCommandSender(sender);
+                return;
+            }
+            SavingConfiguration configuration = ConfigurationManager.getConfiguration();
+            var profiles = serialPlayer.getProfiles();
+            var serialProfile = new SerialProfile(UUID.randomUUID().toString(),configuration.getRandomProfileName(profiles.stream().map(SerialProfile::getProfileName).toList()), "", false);
+            profiles.add(serialProfile);
+            BlobLibMessageAPI.getInstance().getMessage("AlternativeSaving.Added-Profile", target)
+                    .modder()
+                    .replace("%profileName%", serialProfile.getProfileName())
+                    .replace("%player%", target.getName())
+                    .get()
+                    .toCommandSender(sender);
+        });
+    }
+
+    public void switchProfile(Command profileCommand){
+        Command command = profileCommand.child("switch");
+        command.setParameters(BukkitCommandTarget.ONLINE_PLAYERS());
+        command.onExecute((permissionMessenger, args) -> {
             CommandSender sender = BukkitAdapter.getInstance().of(permissionMessenger);
             Player target = BukkitCommandTarget.ONLINE_PLAYERS().parse(args[0]);
             if (target == null) {
