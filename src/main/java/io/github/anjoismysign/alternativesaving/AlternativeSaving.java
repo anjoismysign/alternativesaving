bbf4321217692170d91e59a71f2389eaeaaf6bc2
anjoismysign <lubensaico@gmail.com>
2026-08-04 21:39:55 -0600

    Integrates Vault profile API by registering AlternativeSavingProfile

--- src/main/java/io/github/anjoismysign/alternativesaving/AlternativeSaving.java
+++ src/main/java/io/github/anjoismysign/alternativesaving/AlternativeSaving.java
@@ -4,8 +4,11 @@ import io.github.anjoismysign.alternativesaving.command.AlternativeSavingCommand
 import io.github.anjoismysign.alternativesaving.configuration.SavingConfiguration;
 import io.github.anjoismysign.alternativesaving.director.SavingManagerDirector;
 import io.github.anjoismysign.alternativesaving.director.manager.ConfigurationManager;
+import io.github.anjoismysign.alternativesaving.profile.AlternativeSavingProfile;
 import io.github.anjoismysign.bloblib.managers.BlobPlugin;
 import io.github.anjoismysign.bloblib.managers.IManagerDirector;
+import net.milkbowl.vault.profile.wrappers.ProfileWrapper;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
@@ -25,6 +28,9 @@ public final class AlternativeSaving extends BlobPlugin {
         SavingManagerDirector director = new SavingManagerDirector(this);
         proxy = director.proxy();
         AlternativeSavingCommand.INSTANCE.load();
+        if (Bukkit.getPluginManager().getPlugin("Vault") != null) {
+            new ProfileWrapper(new AlternativeSavingProfile()).registerProviders();
+        }
     }
 
     public void info(@NotNull String message){
